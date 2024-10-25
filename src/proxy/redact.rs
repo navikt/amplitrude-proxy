51aeefcd02d98958e0c65b04b618c138cfcb84f7
carl hedgren <carl.hedgren@nav.no>
2024-10-25 13:52:33 +0200

    don't redact client_upload_time

--- src/proxy/redact.rs
+++ src/proxy/redact.rs
@@ -77,6 +77,7 @@ pub fn traverse_and_redact(value: &mut Value) {
 					|| key == "device_id"
 					|| key == "website"
 					|| key.contains("insert_id")
+					|| key == "client_upload_time"
 				{
 					continue;
 				}
