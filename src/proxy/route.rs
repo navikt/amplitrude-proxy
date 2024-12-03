b161eb293cf697f52eaecb1dc1301add64a7edd5
carl hedgren <carl.hedgren@nav.no>
2024-12-03 12:07:05 +0100

    Add http2/http1 metrics

--- src/proxy/route.rs
+++ src/proxy/route.rs
@@ -1,7 +1,5 @@
 use std::fmt::{self, Display, Formatter};
 
-use crate::metrics::{COLLECT, COLLECT_AUTO};
-
 #[derive(Debug, PartialEq)]
 pub enum Route {
 	Amplitude(String),
@@ -12,18 +10,6 @@ pub enum Route {
 impl Display for Route {
 	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
 		write!(f, "{self:?}")
-	}
-}
-
-pub fn match_route(path: String) -> Route {
-	if path.starts_with("/collect-auto") {
-		COLLECT_AUTO.inc();
-		Route::AmplitudeCollect(path)
-	} else if path.starts_with("/collect") {
-		COLLECT.inc();
-		Route::Amplitude(path)
-	} else {
-		Route::Unexpected(path)
 	}
 }
 
